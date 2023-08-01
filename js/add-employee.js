4b078bc84d4f1ea9807cab132cc567fd72149559
Paris Klas <138509092+parisklas@users.noreply.github.com>
2023-08-01 17:26:02 -0400

    Almost there.
    Completed most of this assignment, but still stuck on the helper function to call html elements from the DOM.

--- js/add-employee.js
+++ js/add-employee.js
@@ -1,21 +1,54 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
+window.addEventListener("load", function () {
+  "use strict";
+  console.log('The page has fully loaded');
+});
 
 // SET WIDTH AND HEIGHT VARIABLES TO 800 x 700
+var width = 800;
+var height = 700;
 
 // RESIZE THE POPUP TO THE SET WIDTH AND HEIGHT
+window.resizeTo(width, height);
 
 // MOVE THE POPUP TO THE MIDDLE OF THE SCREEN
+window.moveTo(((window.screen.width - width) / 2) , ((window.screen.height - height) / 2));
 
 // CREATE A HELPER FUNCTION TO RETRIEVE THE HTML ELEMENTS FROM THE DOM
-
+var $ = function (selector) {
+  return document.querySelector(selector);
+};
 // HANDLE THE CANCEL BUTTON. WHEN THE USER CLICKS THIS BUTTON, CLOSE THE WINDOW
+var cancelBtn = window.document.getElementById("cancel");
+
+cancelBtn.addEventListener("click", function () {
+  console.log("Cancelled");
+  window.close();
+});
 
 // HANDLE THE SUBMISSION OF THE FORM AND THEN IMMEDIATELY PREVENT THE SUBMISSION
+let empForm = document.getElementById("empForm");
+empForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("form submitted");
+});
 
 // CREATE 5 VARIABLES FOR ID, NAME, EXT, EMAIL, AND DEPT
 // SET THOSE VARIABLES TO WHATEVER THE USER ENTERS INTO THE FORM ELEMENTS
+var id = document.getElementById("id").value;
+var name = document.getElementById("name").value;
+var ext = document.getElementById("extension").value;
+var email = document.getElementById("email").value;
+var dept = document.getElementById("department").value;
 
 // GET THE LOGINDETAILS OUTPUT ELEMENT FROM THE PARENT PAGE
+window.addEventListener('load', () => {
+  let parentoutput = window.opener.document.getElementById("loginDetails");
+  parentoutput.innerHTML = id;
+  parentoutput.innerHTML = (`${name}`);
+
+})
+
 
 // SET THE TEXT OF THE LOGINDETAILS ELEMENT TO THE ABOVE SET VARIABLES
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
+window.addEventListener("load", function () {
+  "use strict";
+  console.log('The page has fully loaded');
+});
 
 // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
+var myBtn = window.document.getElementById("btnAddEmployee");
 
-// OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
+myBtn.addEventListener("click", function () {
+  console.log("The button was clicked");
+  // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
+  window.open("add-employee.html", "addEmp", "width=400,height=300,resizable=yes");
+});
+
